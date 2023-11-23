31df3cf13bedae491c75baa2d0b086567e1d281d
hauntstg <74162751+hauntstg@users.noreply.github.com>
2023-11-23 18:36:33 +0700

    Create home.js

---
+++ scripts/home.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const loginForm = document.getElementById('login-modal');
+const mainForm = document.getElementById('main-content');
+const logoutBtn = document.getElementById('btn-logout');
+const welcomeHtml = document.getElementById('welcome-message');
+
+const KEY_CURRENTUSER = 'CURRENT_USER';
+const currentUser = JSON.parse(getFromStorage(KEY_CURRENTUSER, '[]'));
+
+// Check Object có empty không
+if (Object.keys(currentUser).length !== 0) {
+  const html = `Welcome ${currentUser.firstName}`;
+  loginForm.style.display = 'none';
+  welcomeHtml.append(html);
+} else mainForm.style.display = 'none';
+
+logoutBtn.addEventListener('click', function () {
+  localStorage.removeItem(KEY_CURRENTUSER);
+  window.location.href = '../index.html';
+});
